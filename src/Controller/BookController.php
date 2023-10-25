5fb7434bea9b76a215d762bd088e5b66fef7a37c
welhazitakwa <welhazitakwa@gmail.com>
2023-10-25 07:53:49 +0200

    edit+published+unpublishedList+aff_published_only

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -26,9 +26,22 @@ class BookController extends AbstractController
 
        #[Route('/Book/list', name:"listBooks" )]
     public function read (BookRepository $bookRepo):Response {
-         $book = $bookRepo->findAll();
+         $book = $bookRepo->findBy(['published' => true]);
+         $published =0;
+         $unpublished =0;
+         for ($i=0; $i<count($book); $i++) {
+            if ($book[$i]->isPublished() == true ) {
+                $published = $published + 1 ;}
+            
+            if ($book[$i]->isPublished() == false ){
+                $unpublished = $unpublished +1 ;
+            }
+         }
+         
          return $this->render ('Book/listBooks.html.twig', [
             "book" => $book ,
+            "published" => $published,
+            "unpublished" => $unpublished
          ] ) ;  
     }
 
@@ -64,6 +77,21 @@ class BookController extends AbstractController
         return $this->redirectToRoute("listBooks");
     }
 
+    #[Route('/Book/Edit/{id}', name: 'book_edit')]
+    public function edit(ManagerRegistry $doctrine, Request $request, $id): Response
+    {
+        $em = $doctrine->getManager();
+        $book = $em->getRepository(Book::class)->find($id);
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $em->flush(); 
+            return $this->redirectToRoute('listBooks'); 
+        }
+        return $this->renderForm('Book/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
 
 
 
